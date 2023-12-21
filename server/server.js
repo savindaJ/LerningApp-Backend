8e991f250841bccf23568b6a93ea4ba9de345963
savindaJ <your-github-thantrige32@gmail.com.noreply.github.com>
2023-12-21 08:34:09 +0530

    add router in server

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
 const app = require('../app');
-
+const router = require('../router/router');
 
 const server = app.listen(3001,'127.0.0.1',()=>{
     console.log("server started !");
 });
+
+app.use('/api',router);
 
